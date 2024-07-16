6ed9c2c1e993ad027d9f0d8ae68582ec60fdd780
lllllghh <g46487929@gmail.com>
2024-07-16 18:31:04 +0800

    print option

--- Practice2/Practice2-1/Program.cs
+++ Practice2/Practice2-1/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Practice2_1
+﻿using System;
+
+namespace Practice2_1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("1. 新增科目(create)\n2. 刪除科目(delete)\n3. 更新科目(update)\n4. 列印成績單(print)\n5. 退出選單(exit)");
         }
     }
 }
