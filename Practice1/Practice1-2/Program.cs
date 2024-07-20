315e70fb2879babed6760a6b8625dc27ed50015e
lllllghh <g46487929@gmail.com>
2024-07-20 22:53:30 +0800

    option 1, 2 optimization

--- Practice1/Practice1-2/Program.cs
+++ Practice1/Practice1-2/Program.cs
@@ -1,48 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+
 namespace Practice1_1
 {
-    internal class Program
-    {
-        struct shopping_cart
-        {
-            public good cord_block;
-            public good belt_block;
-            public good compass_block;
-        }
-
+    internal class Program {
         struct good
         {
-            public  string name;
+            public string name;
             public int price;
+            public int stock;
             public int amount;
             public int total_price;
-            public int stock;
+        }
+            
+        static List<good> item_list = new List<good>{
+            new good{name = "潛水相機防丟繩", price = 199, stock = 10},
+            new good{name = "潛水配重帶", price = 460, stock = 10},
+            new good{name = "潛水作業指北針", price = 1100, stock = 10}
+        };
+
+        class shopping_cart
+        {
+            private Dictionary<good, uint> cart_items;
+
+            public bool add_item(int item_id, uint count) {
+                var current_item = item_list[item_id];
+                cart_items.TryGetValue(current_item, out uint original_count);
+                if (original_count + count > current_item.stock)
+                    return false;
+                cart_items[current_item] = original_count + count;
+                return true;
+            }
+        }
+
+        private static shopping_cart current_user;
+
+        static void print_all_items(bool print_only_name = false) {
+            if (!print_only_name)
+                Console.WriteLine("列表:\n商品名稱 商品單價");
+            for (int i = 0; i < item_list.Count; ++i) {
+                var item = item_list[i];
+                if (print_only_name)
+                    Console.Write("({0}) {1} ", i + 1, item.name);
+                else
+                    Console.WriteLine("{0}. {1} NTD${2}", i + 1, item.name, item.price);
+            }
         }
         
         
-        public static void Main(string[] args)
-        {
-            shopping_cart buyer_1;
-            good safety_cord, weight_belt, compass;
-            safety_cord.name = "潛水相機防丟繩";
-            safety_cord.price = 199;
-            safety_cord.amount = 0;
-            safety_cord.total_price = 0;
-            safety_cord.stock = 1;
-            weight_belt.name = "潛水配重帶";
-            weight_belt.price = 460;
-            weight_belt.amount = 0;
-            weight_belt.total_price = 0;
-            weight_belt.stock = 2;
-            compass.name = "潛水作業指北針";
-            compass.price = 1100;
-            compass.amount = 0;
-            compass.total_price = 0;
-            compass.stock = 1;
-            buyer_1.cord_block = safety_cord;
-            buyer_1.belt_block = weight_belt;
-            buyer_1.compass_block = compass;
+        public static void Main(string[] args) {
 
             void show_cart_list(shopping_cart buyer)
             {
@@ -85,40 +92,22 @@ namespace Practice1_1
                 var option = int.Parse(Console.ReadLine());
                 switch (option) {
                     case 1:
-                        Console.WriteLine("列表:\n商品名稱 商品單價");
-                        Console.WriteLine("1.潛水相機防丟繩 (TWD)199");
-                        Console.WriteLine("2.潛水配重帶 (TWD)460");
-                        Console.WriteLine("3.潛水作業指北針 (TWD)1100");
-                        shopping();
+                        print_all_items();
                         break;
                     case 2:
-                        Console.WriteLine("(1)潛水相機防丟繩 (2)潛水配重帶 (3)潛水作業指北針");
-                        Console.Write("輸入數字選擇商品: ");
-                        var type = int.Parse(Console.ReadLine());
-                        Console.Write("輸入數量: ");
-                        var unit = int.Parse(Console.ReadLine());
-                        switch (type) {
-                            case 1:
-                                buyer_1.cord_block.amount += unit;
-                                buyer_1.cord_block.total_price += unit * safety_cord.price;
-                                shopping();
+                        int type;
+                        while (true) {
+                            print_all_items(true);
+                            Console.Write("\n輸入數字選擇商品: ");
+                            type = int.Parse(Console.ReadLine());
+                            if (type < item_list.Count && type > 0)
                                 break;
-                            case 2:
-                                buyer_1.belt_block.amount += unit;
-                                buyer_1.belt_block.total_price += unit * weight_belt.price;
-                                shopping();
-                                break;
-                            case 3:
-                                buyer_1.compass_block.amount += unit;
-                                buyer_1.compass_block.total_price += unit * compass.price;
-                                shopping();
-                                break;
-                            default:
-                                Console.WriteLine("error");
-                                shopping();
-                                break;
+                            Console.WriteLine("找不到商品{0}，請重新輸入。", type);
                         }
-
+                        Console.Write("輸入數量: ");
+                        var unit = uint.Parse(Console.ReadLine());
+                        if (!current_user.add_item(type - 1, unit))
+                            Console.WriteLine("Too many items!!!");
                         break;
                     case 3:
                         show_cart_list(buyer_1);
