7322b056626a4deb118ae225efdec279af16165b
lllllghh <g46487929@gmail.com>
2024-07-25 18:06:10 +0800

    create and delete work well

--- Practice2/Practice2-1/Program.cs
+++ Practice2/Practice2-1/Program.cs
@@ -14,7 +14,7 @@ namespace Practice2_1
             {
                 Console.WriteLine("我的成績單:\n編號\t科目代碼\t分數\t等第\t學分數");
                 foreach (var trans in grade_report) {
-                    Console.WriteLine(trans.index + "\t" + trans.class_code + "\t" + trans.grade + "\t" + trans.grade + "\t" + trans.credit);
+                    Console.WriteLine(trans.index + "\t" + trans.class_code + "\t\t" + trans.grade + "\t" + trans.grade + "\t" + trans.credit);
                 }
             }
         }
@@ -41,9 +41,6 @@ namespace Practice2_1
                 var method = Console.ReadLine();
                 if (method.StartsWith("create")) {
                     string[] create_array = method.Split(' ');
-                    foreach (var VARIABLE in create_array) {
-                        Console.WriteLine(VARIABLE);
-                    }
                     transcript transcript_1 = new transcript { index = 0, class_code = "0", grade = 0, level = "0", credit = 0 };
                     transcript_1.class_code = create_array[1];
                     transcript_1.grade = int.Parse(create_array[2]);
@@ -54,21 +51,22 @@ namespace Practice2_1
                 }
                 else if (method.StartsWith("delete")) {
                     string[] delete_array = method.Split(' ');
-                    foreach (var item in student_1.grade_report ) {
-                        if (item.class_code == delete_array[1]) {
-                            student_1.grade_report.Remove(item);
-                        }
-                        else {
-                            Console.WriteLine("error");
-                            break;
-                        }
-                            
+                    var itemToRemove = student_1.grade_report.Find(item => item.class_code == delete_array[1]);
+                    if (itemToRemove.class_code != null)  
+                    {
+                        student_1.grade_report.Remove(itemToRemove);
+                        student_1.print_grade_report();
+                    }
+                    else {
+                        Console.WriteLine("error");
                     }
                 }
-                else if(Console.ReadLine().StartsWith("print"))
+                else if(method.StartsWith("print"))
                     student_1.print_grade_report();
-                else 
+                else if (method == "exit")
                     return false;
+                else 
+                    Console.WriteLine("error");
                 
                 return true;
             }
