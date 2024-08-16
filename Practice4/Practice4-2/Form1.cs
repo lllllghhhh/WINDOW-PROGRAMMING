74c7ce85a527659bb83ed52edb95a3396693d067
lllllghh <g46487929@gmail.com>
2024-08-16 17:33:41 +0800

    finish market

--- Practice4/Practice4-2/Form1.cs
+++ Practice4/Practice4-2/Form1.cs
@@ -16,5 +16,32 @@ namespace Practice4_2
 		{
 			InitializeComponent();
 		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var seed_amount = int.Parse(seed.Text);
+			var f_amount = int.Parse(fertilizer.Text);
+			var fruit_amount = int.Parse(fruit.Text);
+			var savings = int.Parse(label2.Text);
+			if (seed_amount > 0 | f_amount > 0 | fruit_amount > 0) {
+				if (seed_check.Checked && seed_amount > 0) {
+					seed_amount++;
+					savings -= 10;
+				}
+				if (f_check.Checked && f_amount > 0) {
+					f_amount++;
+					savings -= 10;
+				}
+				if (fruit_check.Checked && fruit_amount > 0) {
+					seed_amount--;
+					savings += 40;
+				}
+
+				seed.Text = seed_amount.ToString();
+				fertilizer.Text = f_amount.ToString();
+				fruit.Text = fruit_amount.ToString();
+				label2.Text = savings.ToString();
+			}
+		}
 	}
 }
